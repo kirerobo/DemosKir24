cd04bd354cfbada785f6bab055189ed1a1cddd7b
kirerobo <kaedekioryu@gmail.com>
2024-07-11 21:46:51 +0700

    Update index.js

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ window.onscroll = function () {
     if (prevScrollpos > currentScrollPos) {
         document.getElementById("nav").style.top = "0";
     } else {
-        document.getElementById("nav").style.top = "-100px"; // Sesuaikan dengan tinggi navigasi Anda
+        document.getElementById("nav").style.top = "-100px";
     }
 
     prevScrollpos = currentScrollPos;
